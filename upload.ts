a4adeb1e74fbb07a852a7e72f48d8d89e3c67452
Koki Takahashi <hakatasiloving@gmail.com>
2020-11-19 11:45:37 +0900

    Add N and Q boards

--- upload.ts
+++ upload.ts
@@ -10,19 +10,30 @@ import firebase from 'firebase-admin';
 
 	const db = firebase.firestore();
 	const Boards = db.collection('crossword_boards');
+	let cnt = 0;
 
 	const files = await fs.readdir('outputs');
 	for (const file of files) {
 		console.log(file);
 		const type = file.match(/^board-(.)-/)![1];
+		if (type !== 'N') {
+			continue;
+		}
+		cnt++;
+		if (cnt > 100) {
+			continue;
+		}
 		const board = await fs.readFile(path.join('outputs', file));
 		const id = path.basename(file, '.txt').replace('board', 'grossword');
-		await Boards.doc(id).set({
-			category: 'grossword',
-			type: `grossword-${type}`,
-			board: board.toString(),
-			used_at: null,
-		});
+		const snapshot = await Boards.doc(id).get();
+		if (!snapshot.exists) {
+			await Boards.doc(id).set({
+				category: 'grossword',
+				type: `grossword-${type}`,
+				board: board.toString(),
+				used_at: null,
+			});
+		}
 	}
 })();
 
